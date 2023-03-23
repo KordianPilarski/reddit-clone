c50e35e9aba97f99e241f6a0daaa6c1423fdb9dd
Kordian <kordian.pilarski@capgemini.com>
2023-03-23 14:42:55 +0100

    im plement communityPage and getServerSideProps

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,36 @@
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import { Community } from "../../../atoms/communitiesAtom";
+import { firestore } from "../../../firebase/clientApp";
+import safeJsonStringify from "safe-json-stringify";
+
+type CommunityPageProps = {
+  communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  return <div>Comm name: {communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: JSON.parse(
+          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+        ),
+      },
+    };
+  } catch (error) {
+    console.log("getServerSideProps error", error);
+  }
+}
+export default CommunityPage;
